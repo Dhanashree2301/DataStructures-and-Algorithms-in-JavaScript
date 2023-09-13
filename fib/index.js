8905a6cfcdc4ef9b9b7e58d00e965a6079a01e8a
Dhanashree2301 <dhanashreepatil2301@gmail.com>
2023-09-13 12:08:43 +0530

    UPDATE committed

--- fib/index.js
+++ fib/index.js
@@ -7,7 +7,38 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+// ---------------------------------------Iterative approach----------------------------------------------
+// function fib(n) {
+//     const result=[0,1];
+//     for(i=2;i<=n;i++)
+//     {
+//         const a=result[i-1];
+//         const b=result[i-2];
+//         result.push(a+b);
+//     }
+//     // console.log(result);
+//     console.log(result[n]);
+// }
+// fib(10);
 
-function fib(n) {}
+// ----------------------------------------------Recursive approach-------------------------------------------
+function fib(n) {
+    if(n<2){
+        return n;
+    }
+    else
+    {
+        return fib(n-1)+fib(n-2);
+}
+}
+const nTerms = 10;
 
+if(nTerms <=0) {
+    console.log('Enter a positive integer.');
+}
+else {
+    for(let i = 0; i < nTerms; i++) {
+        console.log(fib(i));
+    }
+}
 module.exports = fib;
